bf596400ba32ec45a66d5e20d13c76069ea3e9fd
Mat Bishop <spam@emgeebee.com>
2015-08-29 18:49:29 +0100

    not sure what these changes are

--- wp-content/themes/jokes/functions.php
+++ wp-content/themes/jokes/functions.php
@@ -1,6 +1,6 @@
 <?php
 	function joke_styles(){
-		wp_enqueue_style('joke-style', get_bloginfo('template_url') . '/style.css',false,'1.1','all');
+		wp_enqueue_style('joke-style', get_stylesheet_directory_uri() . '/style.css',false,'1.1','all');
 	}
 	add_action('wp_head','joke_styles');
 ?>
